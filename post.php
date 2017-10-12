bc002966937e93b4e3d2eb6aed37dceb01308353
AT\M.S.Odoevsky <odoevsky.maksim@urfu.ru>
2017-10-12 16:58:09 +0500

    polytics + form + php mailer

---
+++ post.php
@@ -0,0 +1,51 @@
+<?
+// ----------------------------конфигурация-------------------------- //
+
+$adminemail="admin@site.ru";  // e-mail админа
+
+
+$date=date("d.m.y"); // число.месяц.год
+
+$time=date("H:i"); // часы:минуты:секунды
+
+$backurl="http://site.ru/index.html";  // На какую страничку переходит после отправки письма
+
+//---------------------------------------------------------------------- //
+
+
+
+// Принимаем данные с формы
+
+$name=$_POST['name'];
+
+$email=$_POST['surname'];
+
+$msg=$_POST['message'];
+
+
+
+
+
+ // Отправляем письмо админу
+
+mail("$adminemail", "$date $time Сообщение
+от $name", "$msg");
+
+
+
+
+
+// Выводим сообщение пользователю
+
+print "<script language='Javascript'><!--
+function reload() {location = \"$backurl\"}; setTimeout('reload()', 6000);
+//--></script>
+
+$msg
+
+<p>Сообщение отправлено!</p>";
+exit;
+
+ }
+
+?>
